78e49949c7bd495995b16a39e9b872a62567ba42
orandov <orandov@gmail.com>
2014-08-04 00:29:58 +0300

    demo client link from client api page

--- Configly.TestClientApp/AppSettings.cs
+++ Configly.TestClientApp/AppSettings.cs
@@ -7,13 +7,13 @@ namespace Configly.TestClientApp
         [DisplayName("Application Version"), DefaultValue(6.9)]
         public double ApplicationVersion { get; set; }
 
-        [DisplayName("EQ Web Service Integration Url"), DefaultValue("http://www.sd-qa.com/eq/SDCorp/SDcorp.asmx")]
+        [DisplayName("EQ Web Service Integration Url"), DefaultValue("http://www.google.com/eq/SDCorp/SDcorp.asmx")]
         public string EQServiceUrl { get; set; }
 
         [DisplayName("Url for common static web resources"), DefaultValue("http://localhost/CommonUI/")]
         public string CommonUIPartialPath { get; set; }
 
-        [DisplayName("FX Portfolio Url"), DefaultValue("www.sd-qa.com/fx/portfolio.asp")]
+        [DisplayName("FX Portfolio Url"), DefaultValue("www.bing.com/fx/portfolio.asp")]
         public string FXPortfolioUrl { get; set; }
     }
 }
